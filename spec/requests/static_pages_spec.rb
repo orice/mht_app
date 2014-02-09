ce1d3467b307f1c22ceb09fffb6280c27667a4bc
jbuckner <jbuckner@wisc.edu>
2014-02-08 18:16:46 -0600

    Finish static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,43 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+  describe "Home page" do
+
+    it "should have the content 'Mindful Health'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Mindful Health')
+    end
+
+    it "should have the title 'Home" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Mindful Health | Home")
+    end
+  end
+
+  describe "Contact page" do
+
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("Mindful Health | Contact")
+    end
+  end
+
+  describe "About page" do
+
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
+    end
+
+  	it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_title("Mindful Health | About Us")
+    end
+  end
+end
